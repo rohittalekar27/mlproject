e5a71445fe7047e73be7a99500558860ca555ac5
Rohit Talekar <talekarrohit27@gmail.com>
2025-08-19 11:38:55 +0530

    EDA And problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,41 +1,19 @@
-import logging
-import os
 import sys
-from datetime import datetime
+from src.logger import logging
 
-# ----------------- Setup Logging -----------------
-# Base directory of project
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Logs directory
-logs_dir = os.path.join(BASE_DIR, "logs")
-os.makedirs(logs_dir, exist_ok=True)
-
-# Log file path
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
-
-# Configure logger
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s] - %(lineno)d - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-    filemode='w'
-)
-
-# ----------------- Custom Exception -----------------
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    error_message = f"Error in file [{file_name}] at line [{line_number}] : {str(error)}"
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
     return error_message
+
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail)
-        logging.error(self.error_message)  # log automatically
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
